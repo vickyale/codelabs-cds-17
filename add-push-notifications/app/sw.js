ac4c657457373cfb572d3d1b60a93423ed042d33
Victoria Ubaldo <viky.ubaldo@gmail.com>
2018-02-11 20:53:54 -0500

    add subscribe and unsubscribe the user

--- add-push-notifications/app/sw.js
+++ add-push-notifications/app/sw.js
@@ -20,3 +20,31 @@
 /* eslint-env browser, serviceworker, es6 */
 
 'use strict';
+
+
+self.addEventListener('push', function(event) {
+  console.log('[Service Worker] Push Received.');
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+  const title = 'Push Codelab';
+  const options = {
+    body: 'Notificación Vicky, si funciona!! T_T .',
+    icon: 'images/icon.png',
+    badge: 'images/badge.png'
+  };
+//self.registration.showNotification(title, options);
+
+const notificationPromise = self.registration.showNotification(title, options);
+event.waitUntil(notificationPromise);
+  //event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('[Service Worker] Notification click Received.');
+
+  event.notification.close();
+
+  event.waitUntil(
+    clients.openWindow('https://developers.google.com/web/')
+  );
+});
